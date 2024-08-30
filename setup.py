44fda11e0093b346dad63919ca35f44e38c811a2
Jaymin Mistry <jaymin.mistry@faculty.ai>
2024-08-30 13:16:48 +0100

    project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="quality_data_pipelines",
+    packages=find_packages(exclude=["quality_data_pipelines_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud"
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
